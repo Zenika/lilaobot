e7fd1c129677bc5966d5d88cb7def47ac37b45a2
SIX Douglas <six.douglas@gmail.com>
2021-07-08 08:47:51 +0200

    Fix project name and remove commented lines in JS (#18)
    Co-authored-by: SIX Douglas <douglas.six@ext.adeo.com>

--- functions/gmail-notifier/index.js
+++ functions/gmail-notifier/index.js
@@ -15,16 +15,8 @@
 
 'use strict';
 
-//var { google } = require('googleapis');
-//const gmail = google.gmail('v1');
 const querystring = require('querystring');
-//const config = require('./config');
 const oauth = require('./lib/oauth');
-//const helpers = require('./lib/helpers');
-//const { PubSub } = require('@google-cloud/pubsub');
-
-// Instantiates a client
-//const pubsub = new PubSub();
 
 /**
  * Request an OAuth 2.0 authorization code
@@ -81,82 +73,3 @@ exports.oauth2callback = (req, res) => {
       res.status(500).send('Something went wrong; check the logs.');
     });
 };
-
-/**
- * Initialize a watch on the user's inbox
- */
-/*
-exports.initWatch = (req, res) => {
-  // Require a valid email address
-  if (!req.query.emailAddress) {
-    return res.status(400).send('No emailAddress specified.');
-  }
-  const email = querystring.unescape(req.query.emailAddress);
-  if (!email.includes('@')) {
-    return res.status(400).send('Invalid emailAddress.');
-  }
-
-  // Retrieve the stored OAuth 2.0 access token
-  return oauth.fetchToken(email)
-    .then(() => {
-      // Initialize a watch
-      gmail.users.watch({
-        auth: oauth.client,
-        userId: 'me',
-        resource: {
-          labelIds: ['INBOX'],
-          topicName: config.TOPIC_GMAIL_NEW_MESSAGE_NAME
-        }
-      });
-    })
-    .then(() => {
-      // Respond with status
-      res.write('Watch initialized!');
-      res.status(200).end();
-    })
-    .catch((err) => {
-      // Handle errors
-      if (err.message === config.UNKNOWN_USER_MESSAGE) {
-        res.redirect('/oauth2init');
-      } else {
-        console.error(err);
-        res.status(500).send('Something went wrong; check the logs.');
-      }
-    });
-};
-*/
-/**
-* Process new messages as they are received
-*/
-/*
-exports.onNewMessage = (message, context) => {
-  // Parse the Pub/Sub message
-  const dataStr = Buffer.from(message.data, 'base64').toString();
-  const dataObj = JSON.parse(dataStr);
-
-  return oauth.fetchToken(dataObj.emailAddress)
-    .then(() => helpers.listMessageIds())
-    .then(res => helpers.getMessageById(res.data.messages[0].id)) // TODO: foreach
-    .then(res => {
-      console.log(res);
-      const topic = pubsub.topic(config.TOPIC_CALL_FOR_OFFER_NAME);
-
-      const messageObject = {
-        data: {
-          message: res.payload.body.data,
-        },
-      };
-      const messageBuffer = Buffer.from(JSON.stringify(messageObject), 'utf8');
-
-      // Publishes a message
-      return topic.publish(messageBuffer);
-    })
-    .then(() => console.info('Message published.'))
-    .catch((err) => {
-      // Handle unexpected errors
-      if (!err.message || err.message !== config.NO_LABEL_MATCH) {
-        console.error(err);
-      }
-    });
-};
-*/
