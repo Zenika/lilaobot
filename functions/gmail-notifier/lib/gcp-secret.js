f7100cfbe6b38bd3348314b411b719c19b5faf1b
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2021-12-15 22:51:14 +0100

    fix: forget to import config

--- functions/gmail-notifier/lib/gcp-secret.js
+++ functions/gmail-notifier/lib/gcp-secret.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const config = require('../config')
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
 const secretManagerServiceClient = new SecretManagerServiceClient()
 
